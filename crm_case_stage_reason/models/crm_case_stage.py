1684cf49e807bbb127470b296915f2cd835a8ad3
Daniel Lago <dlagosuarez@gmail.com>
2015-08-06 18:31:40 +0200

    [FIX] Modify create and write to allow constraint

--- crm_case_stage_reason/models/crm_case_stage.py
+++ crm_case_stage_reason/models/crm_case_stage.py
@@ -16,7 +16,7 @@ class CrmCaseStage(models.Model):
     """ Extends crm.case.stage model adding it reasons
 
     Fields:
-      available_reason_ids: available reasons for each stage of case
+      crm_reason_ids: available reasons for each stage of case
       default_reason_id: default reason for each stage of case
     """
 
@@ -50,7 +50,7 @@ class CrmCaseStage(models.Model):
     )
 
     default_crm_reason_id = fields.Many2one(
-        'crm.stage.reason',
+        comodel_name='crm.stage.reason',
         string='Default reason',
         select=True,
         ondelete='restrict',
@@ -87,6 +87,53 @@ class CrmCaseStage(models.Model):
         cr.execute(self._sql_default_crm_reason_id)
 
         return result
+
+    @api.multi
+    def write(self, values):
+        """
+            Update all record(s) in recordset, with new value comes as {values}
+            return True on success, False otherwise
+
+            @param values: dict of new values to be set
+            @return: True on success, False otherwise
+        """
+        result = True
+
+        if self._both_reasons_changed(values):
+            crm_reason_ids = values.pop('crm_reason_ids')
+            result = super(CrmCaseStage, self).write(
+                {'crm_reason_ids': crm_reason_ids})
+
+        return result and super(CrmCaseStage, self).write(values)
+
+    @api.model
+    def create(self, values):
+        """
+            Create a new record for a model CrmCaseStage
+
+            @param values: provides a data for new record
+            @return: returns a id of new record
+        """
+        default_crm_reason_id = None
+
+        if self._both_reasons_changed(values):
+            default_crm_reason_id = values.pop('default_crm_reason_id')
+
+        result = super(CrmCaseStage, self).create(values)
+
+        if result and default_crm_reason_id:
+            result.write({'default_crm_reason_id': default_crm_reason_id})
+
+        return result
+
+    # ----------------------------- NEW METHODS -------------------------------
+
+    def _both_reasons_changed(self, values):
+        return \
+            'default_crm_reason_id' in values and \
+            values['default_crm_reason_id'] and \
+            'crm_reason_ids' in values and \
+            values['crm_reason_ids']
 
     # ----------------------------- SQL QUERIES -------------------------------
 
